c569434600492b9ad0ef39d6e9f52ba4696767f2
onchangelz <2235841385@qq.com>
2015-08-23 17:57:45 +0800

    scrollLoading with jQuery

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 $(window).on('load',function(){
 	waterFall();
+	var dataInt={"data":[{"src":"0.jpg"},{"src":"1.jpg"},{"src":"2.jpg"},{"src":"3.jpg"}]};
+	$(window).on('scroll',function(){
+		if(checkScrollSlide()){
+			$.each(dataInt.data,function(key,value){
+				var oBox=$('<div>').addClass('box').appendTo($('#main'));
+				var oPic=$('<div>').addClass('pic').appendTo(oBox);
+				$('<img>').attr('src','images/'+$(value).attr('src')).appendTo(oPic);
+			});
+			waterFall();
+		}
+	});
 });
 
 function waterFall() {
@@ -25,4 +36,13 @@ function waterFall() {
 		}
 	});
 	// console.log(hArr);
+}
+
+function checkScrollSlide() {
+	var $lastBox=$('#main>div').last();
+	var lastBoxDis=$lastBox.offset().top+Math.floor($lastBox.outerHeight()/2);
+	var scrollTop=$(window).scrollTop();
+	var documentH=$(window).height();
+	return (lastBoxDis<scrollTop+documentH)?true:false;
+
 }
